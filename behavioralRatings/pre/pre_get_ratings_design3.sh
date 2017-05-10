7be2c89f64efc3be9a9ac769e8f4d4c6a2a70a2c
megq <megq@upenn.edu>
2017-05-10 12:25:20 -0400

    add pre scripts

---
+++ behavioralRatings/pre/pre_get_ratings_design3.sh
@@ -0,0 +1,34 @@
+#this script extracts pre ratings data for each subject in design 3 and outputs data to both subject specific and aggregate files 
+
+#create a variable which gets today's date
+date=`date +%Y-%m-%d`
+
+#loop through the design 3 subjects and
+for i in `cat /data/joy/BBL/studies/conte/subjectData/design3FullSubjectList.txt`
+do
+
+#create a variable which gets the bblid and scanid and prints the scanid to the screen
+bblid=`echo $i | cut -d "_" -f 1`
+scanid=`echo $i | cut -d "_" -f 2`
+
+echo "Processing subject........" $scanid
+
+#find the file for the subject's ratings and faces
+path=`ls -d /data/joy/BBL/studies/conte/processedData/$bblid/*$scanid/associated_files/ratings/pre`
+rating=`ls -d /data/joy/BBL/studies/conte/rawData/$bblid/*$scanid/associated_files/ratings/pre/*"$scanid"*Pre_task_faces*.log` 
+
+#run the R script that parses the log file and gets the ratings responses for each subject
+/import/monstrum2/Applications/R3.2.3/bin/R --file=/data/joy/BBL/projects/conteReproc2017/behaviroalRatings/pre/pre_parse_logfiles_ratings_design3.R --slave --args "$rating" "$scanid" "$path"
+
+#create a file called pre_ratings_date.csv which gets the following column headers
+echo "Subject,Timepoint,Answer.quest29,Face.quest29,Answer.quest30,Face.quest30,Answer.quest31,Face.quest31,Answer.quest32,Face.quest32,Answer.quest37,Face.quest37,Answer.quest38,Face.quest38,Answer.quest39,Face.quest39,Answer.quest40,Face.quest40" > //data/joy/BBL/studies/conte/subjectData/ratingsPre/pre_ratings_"$date".csv
+done
+
+#for every individual's ratings file, append to the aggregate csv file
+for k in $( ls -d /data/joy/BBL/studies/conte/processedData/$bblid/*$scanid/associated_files/ratings/pre/*_pre_ratings_data.csv ) ;do
+tail -1 "$k" | cut -d "," -f 2-100 >> data/joy/BBL/studies/conte/subjectData/ratingsPre/pre_ratings_"$date".csv
+done
+
+
+
+
---
+++ behavioralRatings/pre/pre_parse_logfiles_ratings_design3.R
@@ -0,0 +1,63 @@
+#this script will parse the pre logfile and extra the pre ratings data and ouptut to a subject specific csv
+
+#####Read in Data
+
+#read in run1 ratings log file, run1 faces/tones log file, scanid, and path
+args<- commandArgs(TRUE)
+data1<- read.table(args[1],fill=T)
+scanid<- as.character(args[2])
+path<- as.character(args[3])
+
+#data1<- read.table("/import/monstrum/conte_815814/subjects/17648_9111/associated_files/ratings/pre/9111-Pre_task_faces_BL_wheel1.log",fill=TRUE)
+#scanid<- "9111"
+#path<- "/import/monstrum/conte_815814/subjects/17648_9111/associated_files/ratings/pre"
+####Convert Logfiles to easily readable csv format for later manipulation
+
+#rename columns
+colnames(data1)<- c("Subject","Trial","Event_Type","Code","Time","TTime")
+#subset columns
+data1<- data1[,c(1,2,4)]
+#create a column called Trial which gets the rows in order (so can re-order data later)
+data1$Trial<- as.numeric(1:nrow(data1))
+
+####Subset and Reshape Question and Answer File 
+
+#find question and answer rows
+q<- grep("quest",data1$Code)
+a<- grep("ans",data1$Code)
+#create two files, one with only question data and one with only answer data
+q2<- as.data.frame(data1[q,])
+a2<- as.data.frame(data1[a,])
+#reorder data files by Trial
+q2<- q2[order(q2$Trial),]
+a2<- a2[order(a2$Trial),]
+#delete unnecessary rows (the pre scan asks about 17 faces, there are only 4 in the task) ######this will need adjusting#########################
+q2<-q2[c(29:32,37:40),]
+a2<-a2[c(29:32,37:40),]
+#combine two data files so that question and answer are next to eachother
+qa<- cbind(q2,a2$Trial,a2$Code)
+#create a timepoint column that gets "pre"
+qa$Timepoint<- "pre"
+#subset data to only Subject, Question, Answer and Timepoint columns
+qa<- qa[,c(1,3,5,6)]
+#Rename Code and a2$Code columns to Question and Answer
+colnames(qa)[2]<- "Question"
+colnames(qa)[3]<- "Answer"
+#add column (called Face) for which face goes with which questions ######this will need adjusting
+qa$Face<- c(rep("face_1077",4),rep("face_1086",4))
+#the answer column needs to be converted to something other than factors
+qa$Answer<- as.character(qa$Answer)
+qa$Answer<- substring(qa$Answer,4)
+qa$Answer<- as.numeric(qa$Answer)
+#because some of the files are incomplete/have missing questions, these will have NA rows. In order to reshape data the Subject column
+#needs to not be NA, so create a variable called subject which gets the Subject number, then fill all NA subject column rows with this
+#subject number
+subject<- as.character(qa$Subject[1])
+qa$Subject[is.na(qa$Subject)]<- subject
+#reshape data so that each question is a column and has the face and answer below it (each subject is in a single row)
+library(reshape2)
+qa<- reshape(qa, timevar="Question",idvar=c("Subject","Timepoint"),direction="wide")
+
+#####Write out data
+
+write.csv(qa,paste(path,"/",scanid,"_pre_ratings_data.csv",sep=""))
